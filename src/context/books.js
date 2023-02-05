5b4ec47625bc7a96a6269a2d138574825a7edc06
KB-Toast <kevin.brusson@gmail.com>
2023-02-05 07:07:03 +0100

    Improving my commit game

---
+++ src/context/books.js
@@ -0,0 +1,71 @@
+import { createContext, useState, useCallback } from "react";
+import axios from "axios";
+
+const BooksContext = createContext();
+
+function Provider({ children }) {
+  const [books, setBooks] = useState([]);
+
+  const fetchBooks = useCallback(async () => {
+    const response = await axios.get("http://localhost:3001/books");
+
+    setBooks(response.data);
+  }, []);
+
+  const editBookById = async (id, newTitle) => {
+    const response = await axios.put(`http://localhost:3001/books/${id}`, {
+      title: newTitle,
+    });
+
+    const updatedBooks = books.map((book) => {
+      if (book.id === id) {
+        return { ...book, ...response.data };
+      }
+      return book;
+    });
+    setBooks(updatedBooks);
+  };
+
+  const deleteBookById = async (id) => {
+    await axios.delete(`http://localhost:3001/books/${id}`);
+    const updatedBooks = books.filter((book) => {
+      return book.id !== id;
+    });
+    setBooks(updatedBooks);
+  };
+
+  const createBook = async (title) => {
+    const response = await axios.post("http://localhost:3001/books", {
+      title,
+    });
+
+    // handler that isn't handleCreateBook for clarity sake, since we're going to have plenty of handlers
+    const updatedBooks = [
+      ...books,
+      response.data,
+      // {
+      //   id: Math.round(Math.random() * 9999),
+      //   title,
+      // },
+    ];
+    // IDs are usually generated from back-end, we don't have that (yet) here, so front-end ID gen
+
+    setBooks(updatedBooks);
+  };
+
+  const valueToShare = {
+    books,
+    deleteBookById,
+    editBookById,
+    createBook,
+    fetchBooks,
+  };
+
+  return (
+    <BooksContext.Provider value={valueToShare}>
+      {children}
+    </BooksContext.Provider>
+  );
+}
+export { Provider };
+export default BooksContext;
